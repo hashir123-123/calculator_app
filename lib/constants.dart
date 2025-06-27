e72f04aa0a676a5f6975049caac50b577ec6fa1d
hashir123-123 <hashirmeraj2018@gmail.com>
2025-06-27 21:09:58 +0500

    constant file change

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const Color gray = Color(0xffa5a5a5);
 const Color corange = Color(0xffffa00a);
 const Color white = Colors.white;
+const Color black = Colors.black;
